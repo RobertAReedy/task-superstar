718858b95e25a875668fb52107248d5c5e469d7d
RobertAReedy <robertandreedy@gmail.com>
2022-08-30 13:55:18 -0400

    mobile functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,6 +160,56 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    // console.log("activate", this);
+  },
+  deactivate: function(event) {
+    // console.log("deactivate", this);
+  },
+  over: function(event) {
+    // console.log("over", event.target);
+  },
+  out: function(event) {
+    // console.log("out", event.target);
+  },
+  update: function(event) {
+    let tempArr = [];
+
+    $(this).children().each(function() {
+      let text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      let date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArr.push({ text: text, date: date});
+    });
+
+    let arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log(ui.draggable.remove());
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
